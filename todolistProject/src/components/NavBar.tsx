a72cb4384a2f7602d554dd87dac64539c847b593
yega <mdolma@ymail.com>
2024-07-09 11:44:08 +0200

    navlink changed

--- todolistProject/src/components/NavBar.tsx
+++ todolistProject/src/components/NavBar.tsx
@@ -1,14 +1,15 @@
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
     return (
       <header id="header" className="d-flex flex-column justify-content-center">
         <nav id="navbar" className="navbar nav-menu">
-          <ul>
-            <li><a href="/" className="nav-link scrollto active"><img src="https://api.iconify.design/material-symbols:home-outline-rounded.svg?color=%23888888" alt="Home" className="h-6 w-6" /><span>Home</span></a></li>
-            <li><a href="/add" className="nav-link scrollto"><img src="https://api.iconify.design/material-symbols:add-circle-rounded.svg?color=%23888888" alt="add"  className="h-6 w-6" /> <span>Add Task</span></a></li>
-            <li><a href="/important" className="nav-link scrollto"><img src="https://api.iconify.design/material-symbols:file-copy-outline.svg?color=%23888888" alt="important" className="h-6 w-6"/><span>Important Tasks</span></a></li>
-            <li><a href="/contact" className="nav-link scrollto"> <img src="https://api.iconify.design/material-symbols:contact-phone-outline-sharp.svg?color=%23888888" alt="contact" className="h-6 w-6" /> <span>Contact</span></a></li>
-          </ul>
+          
+            <NavLink to="/" className="nav-link scrollto active"><img src="https://api.iconify.design/material-symbols:home-outline-rounded.svg?color=%23888888" alt="Home" className="h-6 w-6" /><span>Home</span></NavLink>
+             <NavLink to="/add" className="nav-link scrollto"><img src="https://api.iconify.design/material-symbols:add-circle-rounded.svg?color=%23888888" alt="add"  className="h-6 w-6" /> <span>Add Task</span></NavLink>
+            <NavLink to="/important" className="nav-link scrollto"><img src="https://api.iconify.design/material-symbols:file-copy-outline.svg?color=%23888888" alt="important" className="h-6 w-6"/><span>Important Tasks</span></NavLink>
+             <NavLink to="/contact" className="nav-link scrollto"> <img src="https://api.iconify.design/material-symbols:contact-phone-outline-sharp.svg?color=%23888888" alt="contact" className="h-6 w-6" /> <span>Contact</span></NavLink>
+          
         </nav>
       </header>
     );
